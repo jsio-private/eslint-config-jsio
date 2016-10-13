021cfa06babfbe9a537876bda430fa85eec5a820
yofreke <sparkyjoe94@aim.com>
2016-10-12 18:16:07 -0700

    no extra quotes

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
 'use strict';
 
 module.exports = {
-  'extends': 'standard',
-  'env': {
-    'browser': true,
-    'es6': true
+  extends: 'standard',
+  env: {
+    browser: true,
+    es6: true
   },
-  'globals': {
-    'angular': false,
-    '$': false,
-    'QRCode': false,
-    '_': false,
-    'io': false
+  globals: {
+    angular: false,
+    $: false,
+    QRCode: false,
+    _: false,
+    io: false
   },
-  'rules': {
-    'semi': 0,
-    'no-trailing-spaces': [2, {
-      'skipBlankLines': false
-    }],
-    'quotes': [2, 'single', 'avoid-escape'],
+  rules: {
+    semi: 0,
+    'no-trailing-spaces': [2, { skipBlankLines: false }],
+    quotes: [2, 'single', 'avoid-escape'],
     'jsx-quotes': [2, 'prefer-single'],
     'no-debugger': 1,
     'no-undef': 2,
     'no-undef-init': 2,
-    'indent': [2, 2, {
-      'SwitchCase': 1
-    }],
-    'camelcase': [2, {
-      'properties': 'never'
-    }],
+    indent: [2, 2, { SwitchCase: 1 }],
+    camelcase: [2, { properties: 'never' }],
     'no-multiple-empty-lines': [2, {
-      'max': 2,
-      'maxBOF': 0,
-      'maxEOF': 1
+      max: 2,
+      maxBOF: 0,
+      maxEOF: 1
     }]
   }
 };
